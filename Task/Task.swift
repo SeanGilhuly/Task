927c7a29de0725ff25ed6ade4de8fc03dd5ab871
SeanGilhuly <seangilhuly@hotmail.com>
2016-05-19 14:20:59 -0600

    Initial Commit
    Updated the storyboard and CoreData

---
+++ Task/Task.swift
@@ -0,0 +1,32 @@
+//
+//  Task.swift
+//  Task
+//
+//  Created by Sean Gilhuly on 5/19/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Task: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+    
+    convenience init(name: String, notes: String? = nil, due: NSDate? = nil, context: NSManagedObjectContext = Stack.sharedStack.managedObjectContext) {
+        
+        // there is no graceful way to respond to a failure on NSEntityDescription.entityForName, force unwrapping and forcing a crash is the desired behavior for this app
+        
+        // designated initializer
+        
+        let entity = NSEntityDescription.entityForName("Task", inManagedObjectContext: context)!
+        
+        self.init(entity: entity, insertIntoManagedObjectContext: context)
+        
+        self.name = name
+        self.notes = notes
+        
+    }
+
+}
